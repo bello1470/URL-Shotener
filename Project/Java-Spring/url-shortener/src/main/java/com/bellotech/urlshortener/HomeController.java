5df0be1c69bcfdb53ae8b70c8acb182430ab7d45
bello1470 <126983812+bello1470@users.noreply.github.com>
2025-04-02 18:42:56 +0100

    First Commit

---
+++ Project/Java-Spring/url-shortener/src/main/java/com/bellotech/urlshortener/HomeController.java
@@ -0,0 +1,17 @@
+package com.bellotech.urlshortener;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+
+    @GetMapping("/")
+    public String home (Model model) {
+
+        model.addAttribute("tittle", "URL Link Shortener");
+        return "index";
+    }
+}
